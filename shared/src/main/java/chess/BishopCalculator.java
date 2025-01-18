430a6cd035bd153a592f3cb71f5bf1933e3dfb48
BenjaminMcNiven <ben.mcniven.3412@gmail.com>
2025-01-17 19:29:06 -0700

    Improved Code Readability and Composition

--- shared/src/main/java/chess/BishopCalculator.java
+++ shared/src/main/java/chess/BishopCalculator.java
@@ -8,67 +8,33 @@ public class BishopCalculator extends MoveCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
-        //Up Left
-        for (int i=1;i<=8;i++){
-            ChessPosition newPosition=new ChessPosition(position.getRow()+i,position.getColumn()-i);
-            if(board.inBounds(newPosition)){
-                if(board.getPiece(newPosition)==null){
-                    moves.add(new ChessMove(position,newPosition));
-                }
-                else{
-                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(position).getTeamColor()){
-                        moves.add(new ChessMove(position,newPosition));
-                    }
-                    break;
-                }
-            }else{break;}
-        }
-        //Up Right
-        for (int i=1;i<=8;i++){
-            ChessPosition newPosition=new ChessPosition(position.getRow()+i,position.getColumn()+i);
-            if(board.inBounds(newPosition)){
-                if(board.getPiece(newPosition)==null){
-                    moves.add(new ChessMove(position,newPosition));
-                }
-                else{
-                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(position).getTeamColor()){
-                        moves.add(new ChessMove(position,newPosition));
-                    }
-                    break;
-                }
-            }else{break;}
-        }
-        //Down Left
-        for (int i=1;i<=8;i++){
-            ChessPosition newPosition=new ChessPosition(position.getRow()-i,position.getColumn()-i);
-            if(board.inBounds(newPosition)){
-                if(board.getPiece(newPosition)==null){
-                    moves.add(new ChessMove(position,newPosition));
-                }
-                else{
-                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(position).getTeamColor()){
-                        moves.add(new ChessMove(position,newPosition));
-                    }
-                    break;
-                }
-            }else{break;}
-        }
-        //Down Right
-        for (int i=1;i<=8;i++){
-            ChessPosition newPosition=new ChessPosition(position.getRow()-i,position.getColumn()+i);
-            if(board.inBounds(newPosition)){
-                if(board.getPiece(newPosition)==null){
-                    moves.add(new ChessMove(position,newPosition));
-                }
-                else{
-                    if(board.getPiece(newPosition).getTeamColor()!=board.getPiece(position).getTeamColor()){
-                        moves.add(new ChessMove(position,newPosition));
-                    }
-                    break;
-                }
-            }else{break;}
-        }
+
+        moves.addAll(iterateMoves(board,position,1,-1));
+        moves.addAll(iterateMoves(board,position,1,1));
+        moves.addAll(iterateMoves(board,position,-1,-1));
+        moves.addAll(iterateMoves(board,position,-1,1));
+
         return moves;
     }
 
+    public Collection<ChessMove> iterateMoves(ChessBoard board, ChessPosition position, int row,int col) {
+        Collection<ChessMove> moves = new ArrayList<>();
+        for (int i = 1; i <= 8; i++) {
+            ChessPosition newPosition = new ChessPosition(position.getRow() + i * row, position.getColumn() + i * col);
+            if (board.inBounds(newPosition)) {
+                if (board.getPiece(newPosition) == null) {
+                    moves.add(new ChessMove(position, newPosition));
+                } else {
+                    if (board.getPiece(newPosition).getTeamColor() != board.getPiece(position).getTeamColor()) {
+                        moves.add(new ChessMove(position, newPosition));
+                    }
+                    return moves;
+                }
+            } else {
+                return moves;
+            }
+        }
+        return moves;
+    }
 }
+
