bd6262666747321313089f3451cc5438016cbc91
BenjaminMcNiven <ben.mcniven.3412@gmail.com>
2025-01-17 20:21:49 -0700

    Improved Code Readability and Composition

--- shared/src/main/java/chess/RookCalculator.java
+++ shared/src/main/java/chess/RookCalculator.java
@@ -9,38 +9,18 @@ public class RookCalculator extends MoveCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> moves = new ArrayList<>();
 
-        moves.addAll(iterateMovesVertical(board,position,1));
-        moves.addAll(iterateMovesVertical(board,position,-1));
-        moves.addAll(iterateMovesHorizontal(board,position,1));
-        moves.addAll(iterateMovesHorizontal(board,position,-1));
+        moves.addAll(iterateMoves(board,position,1,0));
+        moves.addAll(iterateMoves(board,position,-1,0));
+        moves.addAll(iterateMoves(board,position,0,1));
+        moves.addAll(iterateMoves(board,position,0,-1));
 
         return moves;
     }
 
-    public Collection<ChessMove> iterateMovesHorizontal(ChessBoard board, ChessPosition position, int direction){
+    public Collection<ChessMove> iterateMoves(ChessBoard board, ChessPosition position, int row,int col){
         Collection<ChessMove> moves = new ArrayList<>();
         for(int i=1; i<=8; i++) {
-            ChessPosition newPosition = new ChessPosition(position.getRow(), position.getColumn() + (i * direction));
-            if (board.inBounds(newPosition)) {
-                if (board.getPiece(newPosition) == null) {
-                    moves.add(new ChessMove(position, newPosition));
-                } else {
-                    if (board.getPiece(newPosition).getTeamColor() != board.getPiece(position).getTeamColor()) {
-                        moves.add(new ChessMove(position, newPosition));
-                    }
-                    return moves;
-                }
-            } else {
-                return moves;
-            }
-        }
-        return moves;
-    }
-
-    public Collection<ChessMove> iterateMovesVertical(ChessBoard board, ChessPosition position, int direction){
-        Collection<ChessMove> moves = new ArrayList<>();
-        for(int i=1; i<=8; i++) {
-            ChessPosition newPosition = new ChessPosition(position.getRow() + (i * direction), position.getColumn());
+            ChessPosition newPosition = new ChessPosition(position.getRow() + (i * row), position.getColumn()+ (i * col));
             if (board.inBounds(newPosition)) {
                 if (board.getPiece(newPosition) == null) {
                     moves.add(new ChessMove(position, newPosition));
