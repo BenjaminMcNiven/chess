5b7c55e0a66087119f9c938fa14f2746cb384235
benjaminmcniven <ben.mcniven.3412@gmail.com>
2025-02-28 09:22:45 -0700

    Removed Log Lines

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -10,14 +10,12 @@ public class MemoryGameDAO implements GameDAO{
     final private HashMap<Integer, GameData> games = new HashMap<>();
 
     public void createGame(GameData newGame){
-        System.out.println(newGame);
         games.put(newGame.gameID(),newGame);
     }
     public GameData getGame(int gameId){
         return games.get(gameId);
     }
     public Collection<GameData> listGames(){
-        System.out.println("List Games DAO: \n"+games.values());
         return games.values();
     }
     public void updateGame(GameData game){
