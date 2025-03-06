9bfdc956897ab3ca8c36b772e9aeafb34604cd19
benjaminmcniven <ben.mcniven.3412@gmail.com>
2025-03-06 03:50:18 -0700

    Added clear, and createGame except for serializing the ChessGame

--- server/src/main/java/dataaccess/MySqlGameDAO.java
+++ server/src/main/java/dataaccess/MySqlGameDAO.java
@@ -2,25 +2,39 @@ package dataaccess;
 
 import model.GameData;
 
-import javax.xml.crypto.Data;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 public class MySqlGameDAO implements GameDAO{
 
-    public MySqlGameDAO() throws DataAccessException {
+    public MySqlGameDAO(){
         configureDatabase();
     }
 
     @Override
     public void clear() {
-
+        String clear = "DELETE FROM games;";
+        try (var conn = DatabaseManager.getConnection(); PreparedStatement statement = conn.prepareStatement(clear)) {
+            statement.executeUpdate();
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void createGame(GameData newGame) {
-
+        String createUser = "INSERT INTO games (gameID,whiteUsername,blackUsername,gameName,game) VALUES (?, ?, ?,?,?);";
+        try (var conn = DatabaseManager.getConnection(); PreparedStatement statement = conn.prepareStatement(createUser)) {
+            statement.setString(1, String.valueOf(newGame.gameID()));
+            statement.setString(2, newGame.whiteUsername());
+            statement.setString(3, newGame.blackUsername());
+            statement.setString(4, newGame.gameName());
+            statement.setString(5, String.valueOf(newGame.game()));
+            statement.executeUpdate();
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
@@ -45,21 +59,39 @@ public class MySqlGameDAO implements GameDAO{
               `whiteUsername` varchar(256) NULL,
               `blackUsername` varchar(256) NULL,
               `gameName` varchar(256) NULL,
-              'game' TEXT NOT NULL
+              `game` TEXT NOT NULL
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
             """
     };
 
-    private void configureDatabase() throws DataAccessException {
-        DatabaseManager.createDatabase();
-        try (var conn = DatabaseManager.getConnection()) {
+    private void configureDatabase() throws RuntimeException {
+        try{
+            DatabaseManager.createDatabase();
             for (var statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
+                var conn = DatabaseManager.getConnection();
+                var preparedStatement = conn.prepareStatement(statement);
+                preparedStatement.executeUpdate();
             }
-        } catch (DataAccessException | SQLException ex) {
-            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        } catch (SQLException | DataAccessException e) {
+            throw new RuntimeException(String.format("Unable to configure database: %s", e.getMessage()));
         }
+    }
+
+    public List<Map<String, Object>> executeQuerySQL(PreparedStatement statement) {
+        List<Map<String, Object>> results = new ArrayList<>();
+        try (var resultSet = statement.executeQuery()) {
+            var metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            while (resultSet.next()) {
+                Map<String, Object> row = new HashMap<>();
+                for (int i = 1; i <= columnCount; i++) {
+                    row.put(metaData.getColumnName(i), resultSet.getObject(i));
+                }
+                results.add(row);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return results;
     }
 }
