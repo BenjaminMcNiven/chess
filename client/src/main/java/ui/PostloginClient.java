5efafb28d3e04272f1302451699627f1575c6b12
benjaminmcniven <ben.mcniven.3412@gmail.com>
2025-03-07 10:56:57 -0700

    Added eval functions for prew and post clients

--- client/src/main/java/ui/PostloginClient.java
+++ client/src/main/java/ui/PostloginClient.java
@@ -1,14 +1,61 @@
 package ui;
 
+import java.util.Arrays;
+
 public class PostloginClient implements Client{
     @Override
     public String help() {
-        return "";
+        return """
+                create <NAME> - to create a game
+                list - to list all games
+                join <ID> [WHITE|BLACK] - to join a game
+                observe <ID> - to spectate a game
+                logout - to return to login
+                quit - to exit
+                help - to see possible commands""";
     }
 
     @Override
     public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "create" -> create(params);
+                case "list" -> list();
+                case "join" -> join(params);
+                case "observe" -> observe(params);
+                case "logout" -> logout();
+                case "quit" -> quit();
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public String create(String[] input){
         return "";
     }
 
+    public String list(){
+        return "";
+    }
+
+    public String join(String[] input){
+        return "";
+    }
+
+    public String observe(String[] input){
+        return "";
+    }
+
+    public String logout(){
+        return "";
+    }
+
+    public String quit(){
+        return "";
+    }
 }
--- client/src/main/java/ui/PreloginClient.java
+++ client/src/main/java/ui/PreloginClient.java
@@ -1,13 +1,16 @@
 package ui;
 
-public class PreloginClient implements Client{
+import java.util.Arrays;
+import server.ServerFacade;
 
-//    private final ServerFacade server;
+public class PreloginClient implements Client {
+
+    //    private final ServerFacade server;
     private final String serverURL;
 
-    public PreloginClient(String serverURL){
+    public PreloginClient(String serverURL) {
 //        server = new ServerFacade(serverUrl);
-        this.serverURL= serverURL;
+        this.serverURL = serverURL;
     }
 
     @Override
@@ -16,12 +19,37 @@ public class PreloginClient implements Client{
                 register <USERNAME> <PASSWORD> <EMAIL> - to create an account
                 login <USERNAME> <PASSWORD> - to play chess
                 quit - to exit
-                help - to see possible commands
-                """;
+                help - to see possible commands""";
     }
 
     @Override
     public String eval(String input) {
+        try {
+            var tokens = input.toLowerCase().split(" ");
+            var cmd = (tokens.length > 0) ? tokens[0] : "help";
+            var params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            return switch (cmd) {
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "quit" -> quit();
+                default -> help();
+            };
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    public String register(String[] inputs){
         return "";
     }
+
+    public String login(String[] inputs){
+
+        return "";
+    }
+
+    public String quit(){
+        return "";
+    }
+
 }
